39333c052667b0a5643cb34ae2532b8c9bbd61e7
samiksha pawar <sameekshapawar06@gmail.com>
2025-07-21 20:40:03 +0530

    home page completed

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -6,6 +6,7 @@ import hero_title from '../../assets/hero_title.png';
 import play_icon from '../../assets/play_icon.png';
 import info_icon from '../../assets/info_icon.png';
 import TitleCards from '../../components/TitleCards/TitleCards';
+import Footer from '../../components/Footer/Footer';
 
 const Home = () => {
   return (
@@ -30,6 +31,13 @@ const Home = () => {
             <TitleCards/>
           </div>
         </div>
+        <div className="more-cards">
+          <TitleCards title={"Blockbuster Movies"}/>
+          <TitleCards title={"Only on Netflix"}/>
+          <TitleCards title={"Upcoming"}/>
+          <TitleCards title={"Top picks for you"}/>
+        </div>
+        <Footer/>
       
     </div>
   )
