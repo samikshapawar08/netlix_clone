db508145f6cc9135da923214cb350840e7d26d87
samiksha pawar <sameekshapawar06@gmail.com>
2025-07-21 22:42:14 +0530

    seventh commit

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,9 +1,32 @@
 import React from 'react'
 import './Login.css';
+import logo from '../../assets/logo.png';
+import { Form } from 'react-router-dom';
 
 const Login = () => {
   return (
-    <div className="login">Login</div>
+    <div className="login">
+      <img src={logo} className='login-logo' alt="" /> 
+      <div className="logo-form">
+        <h1>Sign Up</h1>
+        <form>
+          <input type="text" placeholder="your name"/>
+          <input type="email" placeholder="email"/>
+          <input type="password" placeholder="password"/>
+          <button>Sign up</button>
+          <div className="form-help">
+            <div className="remember">
+              <input type="checkbox"/>
+              <label htmlFor="">Remember Me</label>
+            </div>
+            <p>Need help?</p>
+            
+
+          </div>
+        </form>
+
+      </div>
+    </div>
   )
 }
 
